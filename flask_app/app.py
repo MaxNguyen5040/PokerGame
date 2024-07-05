7785483f99ac65090922fbd5e2e1c42f3f07cb4b
MaxNguyen5040 <maxnguyen3012@gmail.com>
2024-07-04 22:06:22 -0500

    Added routes for standard actions, and updated html accordingly.

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,7 +4,9 @@ import random
 app = Flask(__name__)
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+pot = 0
 
+#App routes
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,9 +20,34 @@ def deal():
 def fold(player_id):
     return f'Player {player_id} folds.'
 
-@app.route('/bet/<int:player_id>/<int:amount>')
-def bet(player_id, amount):
-    return f'Player {player_id} bets ${amount}.'
+@app.route('/bet/<int:amount>')
+def bet(amount):
+    global pot
+    pot += amount
+    return f'Bet ${amount} added to the pot. Total pot: ${pot}.'
+
+@app.route('/get_pot')
+def get_pot():
+    global pot
+    return f'Current pot size: ${pot}.'
+
+@app.route('/start_round')
+def start_round():
+    return 'Round started. Cards dealt.'
+
+@app.route('/flop')
+def flop():
+    return 'Flop round.'
+
+@app.route('/turn')
+def turn():
+    return 'Turn round.'
+
+@app.route('/river')
+def river():
+    return 'River round.'
+
+#Poker Game methods
 
 def create_deck():
     return [(rank, suit) for suit in suits for rank in ranks]
